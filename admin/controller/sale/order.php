5f4f2f941e2cf0f38b815dc13ee5815200a57543
Graeme Leighfield <graeme@gelstudios.co.uk>
2013-06-25 10:17:58 +0100

    Custom Address rendering on invoices

--- admin/controller/sale/order.php
+++ admin/controller/sale/order.php
@@ -2414,7 +2414,8 @@ class ControllerSaleOrder extends Controller {
 				if ($order_info['shipping_address_format']) {
 					$format = $order_info['shipping_address_format'];
 				} else {
-					$format = '{firstname} {lastname}' . "\n" . '{company}' . "\n" . '{address_1}' . "\n" . '{address_2}' . "\n" . '{city} {postcode}' . "\n" . '{zone}' . "\n" . '{country}';
+					//$format = '{firstname} {lastname}' . "\n" . '{company}' . "\n" . '{address_1}' . "\n" . '{address_2}' . "\n" . '{city} {postcode}' . "\n" . '{zone}' . "\n" . '{country}';
+                    $format = '{firstname} {lastname}' . "\n" . '{company}' . "\n" . '{address_1}' . "\n" . '{address_2}' . "\n" . '{city}' . "\n" . '{zone}' . "\n" . '{postcode}' . "\n" . '{country}';
 				}
 
 				$find = array(
